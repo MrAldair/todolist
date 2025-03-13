c2382c6e532ea9f854afd1ae11a150338ac98f97
OCSYOLISoftware <antonio.cerezo@outpatientcallsolutions.com>
2025-03-13 13:07:32 -0700

    Se creo la pantalla de log in y create user, base de datos. Ya se pueden dar de alta usuarios y cifrar las contraseñas

---
+++ model/handle_db.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+#----------------Database Conexion----------------
+class HandleDB():
+    def __init__(self, db_path="./task_data.db"):
+        self.db_path = db_path
+        
+#----Crear una nueva conexion para cada solicitud
+    def _connect(self):
+        return sqlite3.connect(self.db_path, check_same_thread=False)
+    
+#------------Users table-------------------------
+
+    def insert(self, data_user):
+        conn = self._connect()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (firstname, lastname, position_id, username, password) VALUES (?, ?, ?, ?, ?)",
+                    (data_user['firstname'], data_user['lastname'], data_user['position_id'], data_user['username'], data_user['password']))
+        conn.commit()
+        conn.close()
+        
+    def get_only(self, data_user):
+        conn = self._connect()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE username = ?", (data_user,))
+        data = cur.fetchone()
+        conn.close()
+        
+    #obtiene el user_id del usuario
+    def get_user_id_by_username(self, username: str):
+        user_data = self.get_only(username)
+        if user_data:
+            return user_data[2]  
+        return None
--- router/signup.py
+++ router/signup.py
@@ -1,11 +1,27 @@
 from fastapi import APIRouter, FastAPI, Request, Form, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from controller.user import User
+from lib.check_passw import check_user
+from model.handle_db import HandleDB
 
-template = Jinja2Templates(directory="./view")
 
 router = APIRouter()
+template = Jinja2Templates(directory="./view")
 
 @router.get('/signup', response_class=HTMLResponse)
 def signup(req: Request):
     return template.TemplateResponse('signup.html', {'request': req})
+
+@router.post('/signup', response_class=HTMLResponse)
+def create_user(firstname: str = Form(), lastname: str = Form(), position_id: int = Form(), username: str = Form(), password: str = Form()):
+    data_user = {
+        'firstname': firstname,
+        'lastname': lastname,
+        'position_id': position_id,
+        'username': username,
+        'password': password,
+    }
+    db = User(data_user)
+    db.create_user()
+    return RedirectResponse('/', status_code=303)
