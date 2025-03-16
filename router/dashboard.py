18d4f90ce240138596ea3f3c314dc737b7cefd2d
MrAldair <antonio.cerezo@cesunbc.edu.mx>
2025-03-16 11:53:46 -0700

    Se creo validacion de log in para pantalla Dashboard

--- router/dashboard.py
+++ router/dashboard.py
@@ -10,6 +10,16 @@ router = APIRouter()
 template = Jinja2Templates(directory="./view")
 db = HandleDB()
 
+#Dependencia para verificar autenticacion
+def get_current_user(req: Request):
+    username = req.cookies.get("username")
+    if not username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="No has iniciado sesion"
+        )
+    return username
+'''
 @router.get('/dashboard', response_class=HTMLResponse)
 def get_dashboard(req: Request):
     return RedirectResponse('/')
@@ -18,6 +28,41 @@ def get_dashboard(req: Request):
 @router.post('/dashboard', response_class=HTMLResponse)
 def post_dashboard(req: Request, username: str = Form(), password: str = Form()):
     verify = check_user(username, password)
-    if verify: 
+    if verify:
         return template.TemplateResponse('dashboard.html', {'request': req, 'data_user': verify})
-    return RedirectResponse('/')
+    return RedirectResponse('/')'
+    '''
+#Dependencia para verificar autenticacion
+def get_current_user(req: Request):
+    username = req.cookies.get("username")
+    if not username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="No has iniciado sesion"
+        )
+    return username
+
+@router.post('/dashboard', response_class=HTMLResponse)
+def post_dashboard(req: Request, username: str = Form(), password: str = Form()):
+    verify = check_user(username, password)
+    if verify:
+        print(f"Inicio de sesion exitoso de usuario: {username}")
+        response = RedirectResponse(url="/dashboard", status_code=status.HTTP_303_SEE_OTHER)
+        response.set_cookie(key="username", value=username)
+        return response
+    else:
+        print(f"Error en la autenticacion para el usuario: {username}")
+        return RedirectResponse(url="/", status_code=303)
+    
+@router.get('/dashboard', response_class=HTMLResponse)
+def get_dashboard(req: Request):
+    username = req.cookies.get('username')
+    if not username:
+        return RedirectResponse(url='/', status_code=303)
+    return template.TemplateResponse(
+        'dashboard.html',
+        {
+            'request':req,
+            'username': username
+        }
+        )
